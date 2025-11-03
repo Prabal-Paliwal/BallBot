3c6c71ef993e4c6def388da235715bb72b096992
Prabal Paliwal <75881401+Prabal-Paliwal@users.noreply.github.com>
2025-11-02 19:33:16 -0500

    Refactor motor control for multiple motors

--- 1motorsetup.ino
+++ 1motorsetup.ino
@@ -1,20 +1,38 @@
-const int dirPin = 26;
-const int stepPin = 25;
-const int enablePin = 27;
+const int numMotors = 2;
+const int dirs[numMotors] = {26, 32};
+const int steps[numMotors] = {25, 18};
+const int ENs[numMotors] = {27, 33};
+
 
 void setup(){
-  pinMode(enablePin, OUTPUT);
-  digitalWrite(enablePin, HIGH);
-  pinMode(dirPin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
+  for(int i = 0; i < numMotors; i++){
+    pinMode(ENs[i], OUTPUT);
+    pinMode(dirs[i], OUTPUT);
+    pinMode(steps[i], OUTPUT);
+  }
   delay(2000);
-  digitalWrite(dirPin, HIGH);
-
 }
 
 void loop(){
-  digitalWrite(stepPin, HIGH);
-  delay(5);
-  digitalWrite(stepPin, LOW);
-  delay(5);
+  if(touchRead(T0) < 50){
+    for(int i = 0; i < numMotors; i++){
+      digitalWrite(ENs[i], HIGH);
+      if(touchRead(T3) < 50){
+        digitalWrite(dirs[i], LOW);
+      }else{
+        digitalWrite(dirs[i], HIGH);
+      }
+      digitalWrite(steps[i], HIGH);
+    }
+      delayMicroseconds(5000);
+      for(int i = 0; i < numMotors; i++){
+      digitalWrite(steps[i], LOW);
+      }
+      delayMicroseconds(5000);
+  }else{
+    for(int i = 0; i < numMotors; i++){
+      digitalWrite(steps[i], LOW);
+      digitalWrite(ENs[i], LOW);
+    }
+  }
 }
